67c9f64ea51815b5995c3bd7b7343ec7a453eb40
okeskiner <ogankeskiner@gmail.com>
2019-04-01 17:29:40 +0300

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "Basic cnBjdXNlcjpON2YwWU9nRHJQOHI=";
+            var decodekey = MakeTest.CustomConvert.Base64Encode(key);
+            Console.WriteLine(decodekey);
+
+            var encodekey = MakeTest.CustomConvert.Base64Decode(decodekey);
+            Console.WriteLine(encodekey);
+
+            Console.ReadKey();
+        }
+    }
+}
